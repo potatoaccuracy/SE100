dd2e7037915f3ba4db0f1e568a0c372239ae98c6
potatoaccuracy <vivandeshpande@gmail.com>
2024-11-11 20:36:57 +0800

    Exercise 5

---
+++ Week 3/exercise3/script.js
@@ -0,0 +1,9 @@
+let money = 6000;
+
+// Write your code below
+
+money += 3000;
+console.log(money);
+
+money *= 3;
+console.log(money);
---
+++ Week 3/exercise5/script.js
@@ -0,0 +1,7 @@
+const companyName = "Metaa"
+
+// Write your code below
+
+let slicedCompanyName = companyName.substring(0, 4);
+console.log(slicedCompanyName);
+console.log(slicedCompanyName.toUpperCase());
